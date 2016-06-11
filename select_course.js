bd663259449d115afbb1ff171f30de59976a7ea0
lovenery <edittye379@gmail.com>
2016-06-12 00:55:00 +0800

    bg

--- select_course.js
+++ select_course.js
@@ -1,3 +1,4 @@
+// 輸入課號
 function I(classname) {
     var limit = course_data[classname].limitCnt;
     var one=0; var two=0;
@@ -11,6 +12,7 @@ function I(classname) {
     console.log("志願序1: "+ one);
     console.log("志願序2: "+ two);
 }
+// 輸入上限人數
 function O(people) {
     var limit = people;
     var one=0; var two=0;
@@ -23,4 +25,15 @@ function O(people) {
     console.log("志願序2中獎率: "+limit/two);
     console.log("志願序1: "+ one);
     console.log("志願序2: "+ two);
+}
+// 選課男女比
+function G() {
+    var boy=0; var girl=0;
+    for (var j = 0; j < $('table#std td:nth-child(6)').length; j++) {
+        var string = $('table#std tr:nth-child('+j+') td:nth-child(6)').text();
+        if (string=='男') { boy++; }
+        if (string=='女') { girl++; }
+    }
+    console.log("男: "+boy+ " , "+"女: "+girl);
+    console.log("比: "+boy/girl);
 }
